670040a4f5d70dbbaca971e4d7dce1645b49101b
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-19 15:12:48 +0100

    Update to ts 2.1

--- scripts/bivio.ts
+++ scripts/bivio.ts
@@ -1,4 +1,3 @@
-/// <reference path="../typings/index.d.ts" />
 export {default as FeatureToggle} from "./FeatureToggleDecorator";
 export {default as FeatureChecker} from "./FeatureChecker";
 import * as Predicates from "./Predicates";
