ee545db7e892c9d7199b14ee402b48c1903511bb
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-20 11:41:52 +0100

    Update dts format

--- declarations/bivio.d.ts
+++ declarations/bivio.d.ts
@@ -1,34 +1,27 @@
-/// <reference path="../typings/index.d.ts" />
+export function FeatureToggle(predicate: CheckPredicate);
 
-declare module bivio {
-
-    export function FeatureToggle(predicate:CheckPredicate);
-
-    export interface CheckPredicate {
-        ():boolean
-    }
-
-    export interface IFeatureChecker {
-        check(feature:any):boolean;
-        canCheck(feature:any):boolean;
-    }
-
-    export class FeatureChecker implements IFeatureChecker {
-        check(feature:any):boolean;
-        canCheck(feature:any):boolean;
-    }
-
-    interface PredicatesStatic {
-        always():boolean;
-        never():boolean;
-        environment(environments:string[]):() => boolean;
-        version(version:string):() => boolean;
-        compose(p1:CheckPredicate, p2:CheckPredicate):() => boolean;
-        negate(predicate:CheckPredicate):() => boolean;
-    }
-
-    export var Predicates:PredicatesStatic;
-
+export interface CheckPredicate {
+    (): boolean
 }
 
-export = bivio;
+export interface IFeatureChecker {
+    check(feature: any): boolean;
+    canCheck(feature: any): boolean;
+}
+
+export class FeatureChecker implements IFeatureChecker {
+    check(feature: any): boolean;
+
+    canCheck(feature: any): boolean;
+}
+
+interface PredicatesStatic {
+    always(): boolean;
+    never(): boolean;
+    environment(environments: string[]): () => boolean;
+    version(version: string): () => boolean;
+    compose(p1: CheckPredicate, p2: CheckPredicate): () => boolean;
+    negate(predicate: CheckPredicate): () => boolean;
+}
+
+export var Predicates: PredicatesStatic;
